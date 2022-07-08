9f599e14cbb3325455adb7454ec4447a119fc8ce
Kevin Albino Demetro <albinodemetrok@gmail.com>
2022-07-08 20:18:37 -0300

    Implementando regras do facilitador

--- controle-gestao-pessoas-api/src/main/java/br/com/senai/controlegestaopessoasapi/entity/Facilitador.java
+++ controle-gestao-pessoas-api/src/main/java/br/com/senai/controlegestaopessoasapi/entity/Facilitador.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.Data;
 
@@ -20,20 +23,38 @@ public class Facilitador {
 	private Integer id;
 	
 	@Column(name="nome_completo")
+	@Max(value = 50, message = "O nome completo deve ter no máxio 50 caracteres")
+	@Min(value = 2, message = "O nome completo deve ter pelo menos 2 caracteres")
+	@NotEmpty(message = "O nome completo é obrigatório")
 	private String nomeCompleto;
 	
 	@Column(name="cpf")
+	@Max(value = 14, message = "O cpf deve ter 14 caracteres")
+	@Min(value = 14, message = "O cpf deve ter 14 caracteres")
+	@NotEmpty(message = "O cpf é obrigatório")
 	private String cpf;
 	
 	@Column(name="rg")
+	@Max(value = 10, message = "O rg deve ter 10 caracteres")
+	@Min(value = 10, message = "O rg deve ter 10 caracteres")
+	@NotEmpty(message = "O cpf é obrigatório")
 	private String rg;
 	
 	@Column(name="formacao")
+	@Max(value = 1000, message = "A formação deve ter no máximo 1000 caracteres")
+	@Min(value = 2, message = "A formação deve ter no mínimo 2 caracteres")
+	@NotEmpty(message = "O cpf é obrigatório")
 	private String formacao;
 	
 	@Column(name="login")
+	@Max(value = 20, message = "O login deve ter no máxio 20 caracteres")
+	@Min(value = 2, message = "O login deve ter pelo menos 2 caracteres")
+	@NotEmpty(message = "O login é obrigatório")
 	private String login;
 	
-	@Column(name="senha")	
+	@Column(name="senha")
+	@Max(value = 10, message = "A senha deve ter no máxio 10 caracteres")
+	@Min(value = 2, message = "A senha deve ter pelo menos 2 caracteres")
+	@NotEmpty(message = "A senha é obrigatória")
 	private String senha;
 }
