7e6053f565c06181930bc7c8d88c6ecd63989318
Kevin Albino Demetro <albinodemetrok@gmail.com>
2022-07-07 21:31:22 -0300

    Base do projeto backend

---
+++ controle-gestao-pessoas-api/src/main/java/br/com/senai/controlegestaopessoasapi/ControleGestaoPessoasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.controlegestaopessoasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGestaoPessoasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGestaoPessoasApiApplication.class, args);
+	}
+
+}
---
+++ controle-gestao-pessoas-api/src/test/java/br/com/senai/controlegestaopessoasapi/ControleGestaoPessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.controlegestaopessoasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGestaoPessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
