a86698c980b6b3bb147b7d768be684cbe1fb7844
Kevin Albino Demetro <albinodemetrok@gmail.com>
2022-07-07 21:47:38 -0300

    Base do projeto frontEnd

---
+++ controle-gestao-pessoas-view/src/main/java/br/com/senai/controlegestaopessoasview/ControleGestaoPessoasViewApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.controlegestaopessoasview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGestaoPessoasViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGestaoPessoasViewApplication.class, args);
+	}
+
+}
---
+++ controle-gestao-pessoas-view/src/test/java/br/com/senai/controlegestaopessoasview/ControleGestaoPessoasViewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.controlegestaopessoasview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGestaoPessoasViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
