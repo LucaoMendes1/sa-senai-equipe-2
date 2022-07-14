a089dbe85e0c57aad30f96a94769bd5395e88b23
Ana Carolina <anacarolina.s.s@hotmail.com>
2022-07-14 19:59:35 -0300

    Criado Treinamento e Facilitador dto e client

---
+++ controle-gestao-pessoas-view/src/main/java/br/com/senai/controlegestaopessoasview/dto/Facilitador.java
@@ -0,0 +1,25 @@
+package br.com.senai.controlegestaopessoasview.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Facilitador {
+
+	@EqualsAndHashCode.Include
+	private Integer id;
+
+	private String nomeCompleto;
+
+	private String cpf;
+
+	private String rg;
+
+	private String formacao;
+
+	private String login;
+
+	private String senha;
+
+}
